0985e6c6f8cee1589f9f85aed5b8536a8bb36996
chrisamrl <88105670+chrisamrl@users.noreply.github.com>
2025-03-23 19:52:32 -0700

    Fixed Github Action Workflow Files to handle possible environment difference

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', 
+  output: 'export',
+  distDir: 'out',
   reactStrictMode: true,
   images: {
-    unoptimized: true,
+    unoptimized: true
   },
   async rewrites() {
     return [
